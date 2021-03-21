db20941777e583077e58856b467274da5d5b3381
Michael O'Connor <mjoc@Michaels-MacBook-Pro.local>
2021-03-21 19:20:55 -0400

    Initial commit of compling code to generate a table of JSON data from a manually created file.  More work to go, but good to get the table rendering.

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: "Id",
+        accessor: "id"
+    },
+    {
+        Header: "First Name",
+        accessor: "firstname"
+    },
+    {
+        Header: "Last Name",
+        accessor: "lastname"
+    },
+    {
+        Header: "Email",
+        accessor: "email"
+    },
+    {
+        Header: "Superpower",
+        accessor: "superpower"
+    },
+    {
+        Header: "Phone Number",
+        accessor: "phonenumber"
+    }
+]
